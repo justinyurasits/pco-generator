a445ffe161c4dbb791b9d538db698ea71bed8290
JustinYurasits <yurasits@gmail.com>
2026-06-19 12:53:34 -0400

    Add CORS support

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ import tempfile
 from flask import Flask, request, jsonify
 import resend
 from generator import generate
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # ---------------------------------------------------------------------------
 # Config
